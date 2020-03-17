bd60299d36ef0fffc1df1278036919b8e28608c8
Randal Kamradt <randykamradt@gmail.com>
2020-03-17 15:24:39 -0700

    fix syntax error

--- incoming-read-service/src/main/java/net/kamradtfamily/incoming/read/service/IncomingReadController.java
+++ incoming-read-service/src/main/java/net/kamradtfamily/incoming/read/service/IncomingReadController.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.kamradtfamily.incoming.contract.IncomingContract;
 import net.kamradtfamily.incoming.contract.IncomingException;
 import net.kamradtfamily.incoming.contract.Input;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
